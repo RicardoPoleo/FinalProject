5cf0bb1490abbe677d38af4401ad8d010b13aede
germansalinas700 <german.salinas@cl.abstracta.us>
2019-08-19 15:47:56 -0400

    Second commit

--- src/entregaFinal/FunctionalTest.java
+++ src/entregaFinal/FunctionalTest.java
@@ -1,11 +1,14 @@
 package entregaFinal;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -19,13 +22,19 @@ public class FunctionalTest {
 	
 	@BeforeClass
 	public static void setUp() throws MalformedURLException {
-		
 		System.setProperty("webdriver.gecko.driver","src//geckodriver.exe");
+		File pathBinary = new File("src//firefox.exe");
+		FirefoxBinary firefoxBinary = new FirefoxBinary(pathBinary);   
+		DesiredCapabilities desired = DesiredCapabilities.firefox();
+		FirefoxOptions options = new FirefoxOptions();
+		desired.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options.setBinary(firefoxBinary));
+		driver = new FirefoxDriver();	
+/*		System.setProperty("webdriver.gecko.driver","src//geckodriver.exe");
 		FirefoxOptions cap = new FirefoxOptions();
 		cap.setCapability("marionette", true);
         driver = new FirefoxDriver(cap);
-        /*    driver = new MarionetteDriver();
-	/*	driver = new FirefoxDriver(); */
+        driver = new MarionetteDriver();
+		driver = new FirefoxDriver(); */
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
 	}
 	
