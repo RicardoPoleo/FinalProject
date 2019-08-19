dc36d3830a2b7625583d317a402819c2d99ad01a
germansalinas700 <german.salinas@cl.abstracta.us>
2019-08-19 15:29:43 -0400

    Second commit

--- src/entregaFinal/FunctionalTest.java
+++ src/entregaFinal/FunctionalTest.java
@@ -1,13 +1,11 @@
 package entregaFinal;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -23,10 +21,9 @@ public class FunctionalTest {
 	public static void setUp() throws MalformedURLException {
 		
 		System.setProperty("webdriver.gecko.driver","src//geckodriver.exe");
-		DesiredCapabilities cap = DesiredCapabilities.firefox();
+		FirefoxOptions cap = new FirefoxOptions();
 		cap.setCapability("marionette", true);
-        cap.setBrowserName("firefox");
-        driver = new FirefoxDriver();
+        driver = new FirefoxDriver(cap);
         /*    driver = new MarionetteDriver();
 	/*	driver = new FirefoxDriver(); */
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
