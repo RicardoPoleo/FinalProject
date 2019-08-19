5483b79afc2ac0f98703970238196666027ce04b
germansalinas700 <german.salinas@cl.abstracta.us>
2019-08-19 12:45:22 -0400

    Second commit

--- src/entregaFinal/FunctionalTest.java
+++ src/entregaFinal/FunctionalTest.java
@@ -1,9 +1,13 @@
 package entregaFinal;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -16,10 +20,16 @@ public class FunctionalTest {
 	protected static String url = "https://opencart.abstracta.us";
 	
 	@BeforeClass
-	public static void setUp(){
+	public static void setUp() throws MalformedURLException {
+		
 		System.setProperty("webdriver.gecko.driver","src//geckodriver.exe");
-		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		DesiredCapabilities cap = DesiredCapabilities.firefox();
+		cap.setCapability("marionette", true);
+        cap.setBrowserName("firefox");
+        driver = new FirefoxDriver();
+        /*    driver = new MarionetteDriver();
+	/*	driver = new FirefoxDriver(); */
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
 	}
 	
 	@BeforeMethod
