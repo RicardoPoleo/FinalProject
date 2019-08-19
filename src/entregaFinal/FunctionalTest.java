7113f5cbcb1fe7aed2af64a814ae555b6f7d8630
germansalinas700 <german.salinas@cl.abstracta.us>
2019-08-19 17:31:38 -0400

    Second commit

--- src/entregaFinal/FunctionalTest.java
+++ src/entregaFinal/FunctionalTest.java
@@ -2,10 +2,14 @@ package entregaFinal;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -21,8 +25,12 @@ public class FunctionalTest {
 	public static void setUp() throws MalformedURLException {
 		File gecko = new File("src//geckodriver.exe");
 		System.setProperty("webdriver.gecko.driver",gecko.getAbsolutePath());
-		driver = new FirefoxDriver();	
-		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS); 
+		FirefoxBinary binary = new FirefoxBinary();
+        binary.addCommandLineOptions("-headless");
+        Map<String, String> environmentVariables = new HashMap<>();
+		environmentVariables.put("DISPLAY",":80");
+		driver = new FirefoxDriver(new GeckoDriverService.Builder().usingFirefoxBinary(binary).withEnvironment(environmentVariables).build());	
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
 	}
 	
 	@BeforeMethod
